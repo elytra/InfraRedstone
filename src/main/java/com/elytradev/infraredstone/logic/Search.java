0d5c1c7f1f89d6360bfd807f93e69e5f5fcc3177
Falkreon <falkreon@gmail.com>
2018-05-08 23:01:29 -0400

    ...hopefully...working...search?

--- src/main/java/com/elytradev/infraredstone/logic/Search.java
+++ src/main/java/com/elytradev/infraredstone/logic/Search.java
@@ -1,8 +1,15 @@
 package com.elytradev.infraredstone.logic;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import com.elytradev.infraredstone.InfraRedstone;
 import com.elytradev.infraredstone.block.ModBlocks;
+import com.google.common.base.Objects;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
@@ -23,7 +30,7 @@ public class Search {
 		IBlockState initialState = world.getBlockState(initialPos);
 		if (initialState.getBlock()==ModBlocks.INFRA_REDSTONE) {
 			//Search!
-			return wireSearch(world, device, initialPos);
+			return wireSearch(world, device, dir);
 		}
 		
 		if (initialState.getBlock() instanceof ISimpleInfraRedstone) {
@@ -42,53 +49,104 @@ public class Search {
 		return world.isBlockIndirectlyGettingPowered(initialPos);
 	}
 	
-	private static int wireSearch(World world, BlockPos device, BlockPos firstWire) {
-		return 0;
+	
+	private static final EnumFacing[] PLANAR_FACINGS = { EnumFacing.NORTH, EnumFacing.EAST, EnumFacing.SOUTH, EnumFacing.WEST };
+	private static int wireSearch(World world, BlockPos device, EnumFacing dir) {
+		int depth = 0;
+		Set<Endpoint> rejected = new HashSet<Endpoint>();
+		Set<BlockPos> traversed = new HashSet<BlockPos>();
+		List<Endpoint> members = new ArrayList<>();
+		List<Endpoint> queue = new ArrayList<>();
+		List<Endpoint> next = new ArrayList<>();
 		
-		//copy-paste scanNetwork from BB while we write out a proper one
-//	    public void scanNetwork(BiPredicate<World, BlockPos> membership, BiPredicate<World, List<BlockPos>> validator) {
-//	        if (!hasWorld()) return;
-//	        if (world.isRemote) return;
-//	        Set<BlockPos> seen = new HashSet<>();
-//	        List<BlockPos> members = new ArrayList<>();
-//	        List<BlockPos> queue = new ArrayList<>();
-//	        queue.add(getPos());
-	//
-//	        onDisassemble(world, members);
-	//
-//	        int itr = 0;
-//	        while (!queue.isEmpty()) {
-//	            if (members.size() > getMaxBlocksPerMultiblock()) {
-//	                setControllerStatus(TileEntityMultiblockController.ControllerStatus.ERRORED, "msg.bb.tooBig");
-//	                return;
-//	            }
-//	            BlockPos pos = queue.remove(0);
-//	            seen.add(pos);
-//	            if (membership.test(world, pos)) {
-//	                //TODO: Replace with generalized neighbor function?
-//	                for (EnumFacing ef : EnumFacing.VALUES) {
-//	                    BlockPos p = pos.offset(ef);
-//	                    if (seen.contains(p)) continue;
-//	                    seen.add(p);
-//	                    queue.add(p);
-//	                }
-	//
-//	                if (!members.contains(pos)) {
-//	                    //TODO: This is where we would do early checks, like "is this another controller?" or "is this malformed?"
-//	                    //(world,pos)->world.getBlock(pos) instanceof IBoilerBlock
-//	                    members.add(pos);
-//	                }
-//	            }
-//	            itr++;
-//	        }
-	//
-//	        if (!validator.test(world, members)) {
-//	            setControllerStatus(TileEntityMultiblockController.ControllerStatus.ERRORED, status);
-//	            return;
-//	        }
-	//
-//	        onAssemble(world, members);
-//	        setControllerStatus(TileEntityMultiblockController.ControllerStatus.ACTIVE, "msg.bb.noIssue");
-//	    }
+		queue.add(new Endpoint(device.offset(dir), dir.getOpposite()));
+		if (device.getY()<255) queue.add(new Endpoint(device.offset(dir).offset(EnumFacing.UP), dir.getOpposite()));
+		if (device.getY()>0)   queue.add(new Endpoint(device.offset(dir).offset(EnumFacing.DOWN), dir.getOpposite()));
+		
+		while(!queue.isEmpty() && !next.isEmpty()) {
+			if (queue.isEmpty()) {
+				depth++;
+				if (depth>63) return 0; //We've searched too far, there's no signal in range.
+				queue.addAll(next);
+				next.clear();
+			}
+			
+			Endpoint cur = queue.remove(0);
+			
+			if (world.isAirBlock(cur.pos)) continue;
+			IBlockState state = world.getBlockState(cur.pos);
+			
+			Block block = state.getBlock();
+			if (block==ModBlocks.INFRA_REDSTONE) {
+				traversed.add(cur.pos);
+				
+				//Add neighbors
+				for(EnumFacing facing : PLANAR_FACINGS) {
+					BlockPos offset = cur.pos.offset(facing);
+					
+					if (offset.getY()<255) next.add(new Endpoint(offset.offset(EnumFacing.UP), facing.getOpposite()));
+					if (offset.getY()>0)   next.add(new Endpoint(offset.offset(EnumFacing.DOWN), facing.getOpposite()));
+					if (facing==cur.facing.getOpposite()) continue; //Don't try to bounce back to the block we came from
+					next.add(new Endpoint(offset, facing.getOpposite()));
+				}
+				
+				continue;
+			}
+			
+			Integer rightHere = valueDirectlyAt(world, cur.pos, cur.facing);
+			if (rightHere!=null) {
+				members.add(cur);
+				rejected.add(cur);
+				continue;
+			}
+		}
+		
+		//Grab the strongest signal
+		int strongest = 0;
+		for(Endpoint cur : members) {
+			int val = valueDirectlyAt(world, cur.pos, cur.facing);
+			strongest = Math.max(val, strongest);
+		}
+		return strongest;
+	}
+	
+	private static Integer valueDirectlyAt(World world, BlockPos pos, EnumFacing dir) {
+		if (world.isAirBlock(pos)) return null;
+		IBlockState state = world.getBlockState(pos);
+		Block block = state.getBlock();
+		if (block==ModBlocks.INFRA_REDSTONE) return null; //wires don't carry power directly
+		if (block instanceof ISimpleInfraRedstone) {
+			return ((ISimpleInfraRedstone)block).getSignalValue(world, pos, state, dir);
+		}
+		TileEntity te = world.getTileEntity(pos);
+		if (te!=null && te.hasCapability(InfraRedstone.CAPABILITY_IR, dir)) {
+			return te.getCapability(InfraRedstone.CAPABILITY_IR, dir).getSignalValue();
+		}
+		return null;
+	}
+	
+	private static class Endpoint {
+		BlockPos pos;
+		EnumFacing facing;
+		
+
+		public Endpoint(BlockPos pos, EnumFacing facing) {
+			this.pos = pos;
+			this.facing = facing;
+		}
+		
+		@Override
+		public int hashCode() {
+			return Objects.hashCode(pos, facing);
+		}
+		
+		@Override
+		public boolean equals(Object other) {
+			if (other==null) return false;
+			if (!(other instanceof Endpoint)) return false;
+			Endpoint otherEnd = (Endpoint)other;
+			return Objects.equal(pos, otherEnd.pos) && Objects.equal(facing, otherEnd.facing);
+		}
+		
 	}
 }
