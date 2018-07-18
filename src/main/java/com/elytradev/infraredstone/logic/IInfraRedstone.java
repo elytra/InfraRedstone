48fd5c3e76ff3075ba6995251135384e925adbd4
Meredith Espinosa <goclonefilms@gmail.com>
2018-07-18 00:13:48 -0700

    extra localization

--- src/main/java/com/elytradev/infraredstone/logic/IInfraRedstone.java
+++ src/main/java/com/elytradev/infraredstone/logic/IInfraRedstone.java
@@ -4,5 +4,10 @@ package com.elytradev.infraredstone.logic;
  * This Capability describes an IR signal value to other InfraRedstone logic tiles.
  */
 public interface IInfraRedstone {
+
+	/**
+	 * @return the signal vlaue output by the module.
+	 * It may be helpful to format the value in binary: `0b00_0000`
+	 */
 	int getSignalValue();
 }
--- src/main/java/com/elytradev/infraredstone/logic/IMultimeterProbe.java
+++ src/main/java/com/elytradev/infraredstone/logic/IMultimeterProbe.java
@@ -4,6 +4,12 @@ import net.minecraft.util.text.TextComponentString;
 
 public interface IMultimeterProbe {
 
+    /**
+     * Adds information for the Multimeter to display on use.
+     * @return the phrase the Multimeter should write.
+     * Should contain the signal output of a Module, along with any other important information.
+     * Signal output should be formated "0b00_0000 (0)"
+     */
     TextComponentString getProbeMessage();
 
 }
